2121a923047c42bef7edf949cb31043c1ce80f36
Dave Zeber <dzeber@mozilla.com>
2016-03-07 16:59:46 -0600

    Updated simulations

--- R/simulations.R
+++ R/simulations.R
@@ -17,8 +17,10 @@ n <- 50
 #n <- 1000
 #n <- 100
 #n <- 200
+
 q <- 0.2
 #q <- 0.1
+
 alpha <- c(
 #    0.000001,
 #    0.000005,
@@ -33,109 +35,26 @@ alpha <- c(
     0.1)
 alpha <- c(alpha, 1:9/10)
 
-pmfs <- allpmfs(n, q)
-pr <- privratio(pmfs)
-quants <- pmfquantile(pmfs, alpha, interpolate = TRUE)
 
-
-## For a given collection size n, plot the privacy ratio as a function of the
-## number of 1s in the synthetic collection s = 0:n, for each number of 1s in
-## the original collection m = 1:n.
-## Optionally indicate the values at a specific quantiles of A(m) for each m.
-## Privacy ratio and quantile values should be supplied as tables of
-## (m, s, pr) and (m, alpha, qs) respectively.
-## If 'segments' is TRUE, connect dots across m values for each alpha.
-## If 'maxovers' is TRUE, plot only the largest pr value if there are mutliple
-## for a given s value and alpha.
-plotpr <- function(pr, quants = NULL, segments = FALSE, maxovers = FALSE) {
-    n <- pr[, max(s)]
-    ## Create the basic plot with separate lines for each m.
-    gp <- ggplot(pr, aes(x = s, y = pr, group = factor(m))) +
-        geom_line(colour = "grey") +
-        ylim(pr[, range(pr)]) +
-        theme(text = element_text(size = 10))
-    if(!is.null(alpha)) {
-        setkey(pr, m, s)
-        setkey(quants, m, qs)
-        ## Look up the privacy ratio values at each s point.
-        dd <- pr[quants, nomatch = 0]
-        ## Optionally only plot the max if there are multiple y values for
-        ## given s.
-        if(maxovers) dd <- dd[, list(pr = max(pr)), by = list(s, alpha)]
-        gp <- gp + geom_point(data = dd, aes(colour = factor(alpha)))
-        ## Add line segments to connect the points if required.
-        if(segments) {
-            ddseg <- dd[, list(x = s[-.N], xend = s[-1], y = pr[-.N],
-                yend = pr[-1]), by = alpha]
-            gp <- gp + geom_segment(data = ddseg, aes(x = x, xend = xend,
-                y = y, yend = yend, colour = factor(alpha)))
-        }
-        ## Add indicators for the max privacy ratio value for each alpha.
-        ddmax <- dd[, .SD[which.max(pr)], by = alpha]
-        gp <- gp + geom_point(data = ddmax) +
-            geom_point(data = ddmax, aes(colour = factor(alpha)), size = 0.5)
-    }
-    gp <- gp + labs(
-        title = paste("Privacy ratio vs. s plotted separately for each m",
-            sprintf("(n = %s, q = %s)", n, q),
-            if(!is.null(alpha)) paste(
-                "\nwith indicators at quantile values for each m",
-                "(max indicated in black)")
-            else ""))
-    if(!is.null(alpha)) gp <- gp + labs(colour = "Quantile prob")
-    gp
-}
-
-pdf("pr-quantiles-n50.pdf", 12, 8)
-plotpr(pr, quants, TRUE, FALSE)
-dev.off()
-
-
-## Make plots for multiple values of n and q.
-#nvals <- c(100, 500)
-#qvals <- c(0.1, 0.2, 0.4)
-#alpha <- c(0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 0.9,
-#    0.00001, 0.00005, 0.000001, 0.000005)
-#qnplots <- setNames(lapply(qvals, function(q) {
-#    setNames(lapply(nvals, function(n) { plotpr(n, q, alpha) }),
-#        as.character(nvals))
-#}), as.character(qvals))
-#
-#pdf("ratio-at-quantiles.pdf", 16, 8)
-#lapply(qnplots, function(pp) { multiplot(plotlist = pp, cols = 2) })
-#dev.off()
-#
-
-
-##########################################################
-##########################################################
-
-## Make plots to explore distributions and quantiles.
-#n <- 50
-n <- 100
-q <- 0.2
-#alpha <- c(0.01, 0.05, 0.1, 0.2, 0.5, 0.9)
-alpha <- c(
-    0.000001,
-    0.000005,
-    0.00001,
-    0.00005,
-    0.0001,
-    0.0005,
-    0.001,
-    0.005,
-    0.01,
-    0.05,
-    0.1,
-    0.99,
-    0.999,
-    0.9999
-    )
-alpha <- c(alpha, 1:9/10)
-
+## Compute pmfs for m = 0,...,n.
 pmfs <- allpmfs(n, q)
-quants <- pmfquantile(pmfs, alpha, interpolate = TRUE)
+## Tabulate distribution (pmf/cdf) over m,s.
+## Creates a table of (m, s, pmf, cdf).
+ddist <- rbindlist(mapply(function(i, v) {
+    data.table(m = i - 1, s = seq_along(v) - 1, pmf = v)
+}, seq_along(pmfs), pmfs, SIMPLIFY = FALSE, USE.NAMES = FALSE))
+ddist[, cdf := cumsum(pmf), by = m]
+## Add shifted versions of CDFs for each m.
+ddist[, ssf := s - floor(m*(1-2*q)), by = m]
+ddist[, ssc := s - ceiling(m*(1-2*q)), by = m]
+
+
+
+## Compute privacy ratio for all s, m = 0,...,n.
 pr <- privratio(pmfs)
+## Compute table of quantile info (distribution values at quantiles).
+quants <- pmfquantile(pmfs, alpha, interpolate = TRUE)
+
 ## Privacy ratio at quantiles.
 setkey(pr, m, s)
 setkey(quants, m, qs)
@@ -143,17 +62,101 @@ prquants <- quants[pr][, list(m, alpha, qs, pr, qs.interp, interp)]
 prquants[, pr.interval := rev(diff(c(rev(pr), 0))), by = m]
 prquants[, pr.interp := pr + interp * pr.interval][, pr.interval := NULL]
 prquants <- prquants[!is.na(alpha)]
-## Prob ratios at quantiles.
-setkey(ddist, m, s)
-ddistquants <- quants[ddist][, list(m, alpha, qs, qs.interp, interp,
-    pmf = i.pmf, cdf = i.cdf)]
-ddistquants[, probratio := 
 
-## Create table of distribution.
-ddist <- rbindlist(mapply(function(i, v) {
-    data.table(m = i - 1, s = seq_along(v) - 1, pmf = v)
-}, seq_along(pmfs), pmfs, SIMPLIFY = FALSE, USE.NAMES = FALSE))
-ddist[, cdf := cumsum(pmf), by = m]
+
+## Difference between shifted distributions, compared to CDF jump sizes.
+ddiff <- ddist[s <= max(ssf[m == n]) & m == 0][, list(s, cdf0 = cdf)]
+setkey(ddiff, s)
+## Compare to F_0 shifted up or down by 1 or 2.
+ddiff[, lcdf0 := c(0, cdf0[-length(cdf0)])][,
+    l2cdf0 := c(0, 0, cdf0[-(length(cdf0) + (-1:0))])][,
+    ucdf0 := c(cdf0[-1], 1)][,
+    u2cdf0 := c(cdf0[-(1:2)], 1, 1)]
+## Consecutive differences for F_0.
+#ddiff[, ldiff := c(cdf[1], diff(cdf))][,
+#    ldiff2 := c(cdf[1:2], diff(cdf, lag = 2))][,
+#    udiff := c(diff(cdf), 1-cdf[length(cdf)])][,
+#    udiff2 := c(diff(cdf, lag = 2), 1-c(tail(cdf, 2)))]
+#setnames(ddiff, "cdf", "cdf0")
+## Compare against shifted versions of F_m.
+srange <- ddiff[, range(s)]
+fshift <- ddist[m > 0 & ssf >= srange[1] & ssf <= srange[2]][,
+    list(m, s = ssf, fcdf = cdf)]
+setkey(fshift, s)
+## Join against table for F_0.
+ddiff <- ddiff[fshift]
+## Same for upper shift, but join on both s and m.
+setkey(ddiff, s, m)
+cshift <- ddist[m > 0 & ssc >= srange[1] & ssc <= srange[2]][,
+    list(m, s = ssc, ccdf = cdf)]
+setkey(cshift, s, m)
+ddiff <- ddiff[cshift]
+
+## Try same thing for PMFs.
+ddiff <- ddist[s <= max(ssf[m == n]) & m == 0][, list(s, pmf0 = pmf)]
+setkey(ddiff, s)
+ddiff[, lpmf0 := c(0, pmf0[-length(pmf0)])]
+## Compare against shifted versions of p_m.
+srange <- ddiff[, range(s)]
+fshift <- ddist[m > 0 & ssf >= srange[1] & ssf <= srange[2]][,
+    list(m, s = ssf, fpmf = pmf)]
+setkey(fshift, s)
+## Join against table for F_0.
+ddiff <- ddiff[fshift]
+## Same for upper shift, but join on both s and m.
+setkey(ddiff, s, m)
+cshift <- ddist[m > 0 & ssc >= srange[1] & ssc <= srange[2]][,
+    list(m, s = ssc, ccdf = cdf)]
+setkey(cshift, s, m)
+ddiff <- ddiff[cshift]
+
+
+
+
+##########################################################
+
+## Plot CDFs for multiple m.
+
+## Plot cdfs for multiple m on the same graph.
+## If shift = TRUE, plots F(x + floor(m(p-q))).
+## If additionally use.ceiling = TRUE, replaces 'floor' with 'ceiling'.
+## Optionally limit the y axis range using 'lower' and 'upper'.
+plotcdfshift <- function(ddist, mvals, shift = FALSE, use.ceiling = FALSE,
+                                                lower = NULL, upper = NULL) {
+    xx <- if(shift) {
+        ## Find the maximum s range to consider over our choice of m.
+        svals <- ddist[m %in% mvals,
+            do.call(seq, as.list(range(if(use.ceiling) ssc else ssf)))]
+        smvals <- expand.grid(svals, mvals)
+        smvals <- data.table(m = smvals[[2]], s = smvals[[1]], key = c("m","s"))
+        setkeyv(ddist, c("m", if(use.ceiling) "ssc" else "ssf"))
+        ddshifted <- ddist[smvals]
+        setnames(ddshifted, if(use.ceiling) "ssc" else "ssf", "ss")
+        ddshifted[, s := NULL][is.na(pmf), pmf := 0][
+            is.na(cdf) & ss < 0, cdf := 0][
+            is.na(cdf), cdf := 1]
+        setnames(ddshifted, "ss", "s")
+        ddshifted
+    } else ddist[m %in% mvals]
+    ylims <- c(0,1)
+    if(!is.null(lower)) ylims[1] <- lower
+    if(!is.null(upper)) ylims[2] <- upper
+    qp <- qplot(s, cdf, colour = factor(m), data = xx, geom = "step") +
+        geom_hline(aes(yintercept = cdf), data = xx[m == min(m)], 
+            colour = "grey30", linetype = "dotted") +
+        scale_y_continuous(breaks = interval.breaks(0.1), limits = ylims)
+    titlestr <- sprintf("CDF of A(m) %%s[n = %s, q = %s]", n, q)
+    if(shift) {
+        qp <- qp + xlim(-5, 25)
+        titlestr <- sprintf(titlestr, sprintf("shifted by %s(m(p-q))",
+            if(use.ceiling) "ceiling" else "floor"))
+    } else {
+        titlestr <- sprintf(titlestr, "")
+    }
+    qp + labs(title = titlestr, colour = "m") +
+        theme(text = element_text(size = 8))
+}
+
 
 ## Plot pmfs by m with quantiles indicated.
 plotpmfs <- function(ddist, quants = NULL) {
@@ -233,12 +236,12 @@ plotscumprob <- function(ddist, ratio = FALSE) {
 plotquants <- function(quants, together = TRUE) {
     dd <- rbind(quants[, list(m, alpha, s = qs, type = "exact")],
         quants[, list(m, alpha, s = qs.interp, type = "interpolated")])
-    aesvals <- list(x = quote(m), y = quote(s))
-    if(together) aesvals$colour <- quote(factor(alpha))
-    gp <- ggplot(dd, aes_(aesvals)) +
+    aesvals <- list(x = "m", y = "s")
+    if(together) aesvals$colour <- "factor(alpha)"
+    gp <- ggplot(dd, do.call("aes_string", aesvals)) +
         geom_line(aes(linetype = type)) +
         geom_point(size = 0.5)
-    if(together) gp <- gp + facet_wrap(~ alpha)
+    if(!together) gp <- gp + facet_wrap(~ alpha)
     gp + theme(text = element_text(size = 8)) +
         labs(title = "Quantile q(m; alpha) for each alpha",
             x = "m", y = "Quantile value (s)", colour = "alpha",
@@ -276,7 +279,7 @@ plotquantcumprobs <- function(quants) {
 
 ## Plot the sequence of privacy ratio values at each quantile over m,
 ## for each alpha.
-plotquantpr <- function(prquants, exact = TRUE) {
+plotquantpr <- function(prquants, exact = TRUE, fixedy = FALSE) {
     ## Add indicator of whether previous exact quantile was same.
     prquants[, sameasprev := c(FALSE, qs[-1] == qs[-.N]), by = alpha]
     dd <- rbind(prquants[, list(m, alpha, pr = pr, sameasprev, type = "exact")],
@@ -290,7 +293,8 @@ plotquantpr <- function(prquants, exact = TRUE) {
         scale_colour_manual(values = c("TRUE" = "red", "FALSE" = "black")) +
         scale_size_manual(values = c("exact" = 1, "interpolated" = 0.5),
             guide = FALSE) +
-        facet_wrap(~ alpha, scales = "free_y") +
+        (if(fixedy) facet_wrap(~ alpha)
+            else facet_wrap(~ alpha, scales = "free_y")) +
         theme(text = element_text(size = 8)) +
         labs(title = sprintf(paste("Privacy ratio value at each quantile",
             "q(m; alpha) for each alpha (n = %s, q = %s)"), n, q),
@@ -298,9 +302,101 @@ plotquantpr <- function(prquants, exact = TRUE) {
             colour = "Q(m) = Q(m-1)?")
 }
 
+## Plot ratios of ratios.
+plotquantprratios <- function(prquants, absval = FALSE) {
+    ## Add indicator of whether previous exact quantile was same.
+    prquants[, sameasprev := c(FALSE, qs[-1] == qs[-.N]), by = alpha]
+    dd <- prquants[, list(m = m[-1], rpr = pr[-1]/pr[-.N],
+        sameasprev = sameasprev[-1]), by = alpha]
+    if(absval) dd[sameasprev == FALSE, rpr := 1/rpr]
+    ## Find max for both cases.
+    ddmax <- dd[, .SD[which.max(rpr)], by = list(alpha, sameasprev)]
+    gp <- ggplot(dd, aes(x = m, y = log(rpr))) + geom_line() +
+        geom_point(aes(colour = sameasprev)) +
+        facet_wrap(~ alpha) +
+        geom_point(data = ddmax, size = 0.5) +
+        theme(text = element_text(size = 8)) +
+        labs(title = sprintf(paste("Log ratio of consecutive privacy ratio",
+            "values at quantiles q(m; alpha) for each alpha (n = %s, q = %s)"),
+                n, q),
+            x = "m", y = "log(PR(q(m+1; alpha); m+1)/PR(q(m+1; alpha); m+1))",
+            colour = "Q(m) = Q(m-1)?")
+    if(!absval)  gp <- gp + geom_hline(yintercept = 0, colour = "grey30")
+    gp
+}
+
 pdf(sprintf("%ss-quantiles.pdf", pvar), 12, 10); gp; dev.off()
 
 pdf("privratio-byquantiles-n100.pdf", 12, 8)
 plotquantpr(prquants, FALSE)
 dev.off()
+
+
+## For a given collection size n, plot the privacy ratio as a function of the
+## number of 1s in the synthetic collection s = 0:n, for each number of 1s in
+## the original collection m = 1:n.
+## Optionally indicate the values at a specific quantiles of A(m) for each m.
+## Privacy ratio and quantile values should be supplied as tables of
+## (m, s, pr) and (m, alpha, qs) respectively.
+## If 'segments' is TRUE, connect dots across m values for each alpha.
+## If 'maxovers' is TRUE, plot only the largest pr value if there are mutliple
+## for a given s value and alpha.
+plotpr <- function(pr, quants = NULL, segments = FALSE, maxovers = FALSE) {
+    n <- pr[, max(s)]
+    ## Create the basic plot with separate lines for each m.
+    gp <- ggplot(pr, aes(x = s, y = pr, group = factor(m))) +
+        geom_line(colour = "grey") +
+        ylim(pr[, range(pr)]) +
+        theme(text = element_text(size = 10))
+    if(!is.null(quants)) {
+        setkey(pr, m, s)
+        setkey(quants, m, qs)
+        ## Look up the privacy ratio values at each s point.
+        dd <- pr[quants, nomatch = 0]
+        ## Optionally only plot the max if there are multiple y values for
+        ## given s.
+        if(maxovers) dd <- dd[, list(pr = max(pr)), by = list(s, alpha)]
+        gp <- gp + geom_point(data = dd, aes(colour = factor(alpha)))
+        ## Add line segments to connect the points if required.
+        if(segments) {
+            ddseg <- dd[, list(x = s[-.N], xend = s[-1], y = pr[-.N],
+                yend = pr[-1]), by = alpha]
+            gp <- gp + geom_segment(data = ddseg, aes(x = x, xend = xend,
+                y = y, yend = yend, colour = factor(alpha)))
+        }
+        ## Add indicators for the max privacy ratio value for each alpha.
+        ddmax <- dd[, .SD[which.max(pr)], by = alpha]
+        gp <- gp + geom_point(data = ddmax) +
+            geom_point(data = ddmax, aes(colour = factor(alpha)), size = 0.5)
+    }
+    gp <- gp + labs(
+        title = paste("Privacy ratio vs. s plotted separately for each m",
+            sprintf("(n = %s, q = %s)", n, q),
+            if(!is.null(alpha)) paste(
+                "\nwith indicators at quantile values for each m",
+                "(max indicated in black)")
+            else ""))
+    if(!is.null(alpha)) gp <- gp + labs(colour = "Quantile prob")
+    gp
+}
+
+pdf("pr-quantiles-n50.pdf", 12, 8)
+plotpr(pr, quants, TRUE, FALSE)
+dev.off()
+
+
+## Make plots for multiple values of n and q.
+#nvals <- c(100, 500)
+#qvals <- c(0.1, 0.2, 0.4)
+#alpha <- c(0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 0.9,
+#    0.00001, 0.00005, 0.000001, 0.000005)
+#qnplots <- setNames(lapply(qvals, function(q) {
+#    setNames(lapply(nvals, function(n) { plotpr(n, q, alpha) }),
+#        as.character(nvals))
+#}), as.character(qvals))
+#
+#pdf("ratio-at-quantiles.pdf", 16, 8)
+#lapply(qnplots, function(pp) { multiplot(plotlist = pp, cols = 2) })
+#dev.off()
+#
 
